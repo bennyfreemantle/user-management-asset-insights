f8dda321548342eff85894524de88e9307380fc9
Ben Freemantle <bennyfreemantle@protonmail.com>
2023-02-10 21:31:33 +0000

    refactor out GET all users and listen for method

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -1,18 +1,45 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { prisma } from "@/lib/prisma";
 import { UserResponse } from "@/types";
+import { Prisma } from "@prisma/client";
+
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<UserResponse>
 ) {
-  try {
-    const users = await prisma.users.findMany();
-    res
-      .status(200)
-      .json({ message: `Successfully retrieved all users`, users: users });
-  } catch (e) {
-    console.log(e);
+  if (req.method === "GET") {
+    try {
+      const users = await prisma.users.findMany();
+      res
+        .status(200)
+        .json({ message: `Successfully retrieved all users`, users: users });
+    } catch (e) {
+      console.log(e);
+    }
+  } else if (req.method === "POST") {
+    const { first_name, last_name, email } = req.body;
+
+    try {
+      await prisma.users.create({
+        data: {
+          first_name,
+          last_name,
+          email,
+        },
+      });
+      res.status(200).json({ message: `Successfully create a new user` });
+    } catch (e) {
+      // Constraint checking, if a user has the same email send a response back
+      // that tells lets them know a user with this email already exists
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        if (e.code === "P2002") {
+          res.json({
+            message: `There is a unique constraint violation, a new user cannot be created with this email`,
+          });
+        }
+      }
+    }
   }
 }
